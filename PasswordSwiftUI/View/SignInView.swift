65bd8ef6ba785cf1acbd548ec22faff0f701b434
Vladimir Proskuirn <waldoproskurin@gmail.com>
2020-03-29 17:11:22 +0300

    work with keyboard

--- PasswordSwiftUI/View/SignInView.swift
+++ PasswordSwiftUI/View/SignInView.swift
@@ -9,6 +9,8 @@ struct SignInView: View {
     @State private var open = false
     @State private var pulsate = false
     
+    @State private var value: CGFloat = 0
+    
     @EnvironmentObject private var session: SessionStore
     
     func Spacing() -> CGFloat {
@@ -33,6 +35,8 @@ struct SignInView: View {
                     PlusButtonAuthView()
                 }
                 .offset(x: -Spacing() * -3.9, y: -Spacing() * 2.9)
+                .offset(y: -self.value + 50)
+                .animation(.spring())
                 
                 VStack {
                     HStack {
@@ -44,6 +48,8 @@ struct SignInView: View {
                             .font(.footnote)
                     }
                     .offset(x: -Spacing() * 0, y: -Spacing() * -5.5)
+                    .offset(y: -self.value + 50)
+                    .animation(.spring())
                     
                     VStack {
                         TextField("Email", text: $email)
@@ -56,13 +62,28 @@ struct SignInView: View {
                     }
                     .frame(width: 322)
                     .offset(y: -Spacing() * -6.0)
+                    .offset(y: -self.value + 50)
+                    .animation(.spring())
+                    .onAppear {
+                        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { (noti) in
+                            let value = noti.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+                            let height = value.height
+                            
+                            self.value = height
+                        }
+                        
+                        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { (noti) in
+                            self.value = 0
+                        }
+                    }
                     
                     if (error != "") {
                         Text(error)
                             .font(.subheadline)
-                            .background(Color.blue)
-                            .foregroundColor(.red)
+                            .foregroundColor(Color(red: 255 / 255, green: 160 / 255, blue: 160 / 255))
                             .offset(y: -Spacing() * -7.5)
+                            .offset(y: -self.value)
+                            .animation(.spring())
                     }
                     
                     Spacer()
@@ -71,6 +92,8 @@ struct SignInView: View {
                         CustomButtonAuthView(action: self.signIn) {
                             Text("Sign In")
                         }
+                        .offset(y: -self.value)
+                        .animation(.spring())
                         .offset(x: -Spacing() * -5.7, y: -Spacing() * 0.0)
                         .shadow(color: Color(red: 147 / 255, green: 156 / 255, blue: 238 / 255),
                                 radius: 180.0)
@@ -82,15 +105,16 @@ struct SignInView: View {
                         }
                         
                         NavigationLink(destination: SignUpView()) {
-                            HStack {
-                                Text("Create account")
-                                    .font(.callout)
-                                    .foregroundColor(Color(red: 255 / 255, green: 160 / 255, blue: 160 / 255))
-                            }
+                            Text("Create account")
+                                .font(.callout)
+                                .foregroundColor(Color(red: 255 / 255, green: 160 / 255, blue: 160 / 255))
                         }
+                        .offset(y: -self.value)
+                        .animation(.spring())
                         .offset(x: -Spacing() * 5.4)
                     }
                     .offset(y: -Spacing() * -1.0)
+                    
                 }
             }
         }
--- PasswordSwiftUI/View/SignUpView.swift
+++ PasswordSwiftUI/View/SignUpView.swift
@@ -9,6 +9,8 @@ struct SignUpView: View {
     @State private var open = true
     @State private var pulsate = true
     
+    @State private var value: CGFloat = 0
+    
     @EnvironmentObject private var session: SessionStore
     
     func signUp() {
@@ -32,6 +34,8 @@ struct SignUpView: View {
                 PlusButtonAuthView()
             }
             .offset(x: -Spacing() * -3.9, y: -Spacing() * 2.9)
+            .offset(y: -self.value + 50)
+            .animation(.spring())
             
             VStack {
                 HStack {
@@ -43,6 +47,8 @@ struct SignUpView: View {
                         .font(.footnote)
                 }
                 .offset(x: -Spacing() * 0, y: -Spacing() * -5.5)
+                .offset(y: -self.value + 50)
+                .animation(.spring())
                 
                 VStack {
                     TextField("Email", text: $email)
@@ -55,22 +61,39 @@ struct SignUpView: View {
                 }
                 .frame(width: 322)
                 .offset(y: -Spacing() * -6.0)
+                .offset(y: -self.value + 50)
+                .animation(.spring())
+                .onAppear {
+                    NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { (noti) in
+                        let value = noti.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+                        let height = value.height
+                        
+                        self.value = height
+                    }
+                    
+                    NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { (noti) in
+                        self.value = 0
+                    }
+                }
                 
                 if (error != "") {
                     Text(error)
                         .font(.subheadline)
-                        .background(Color.blue)
-                        .foregroundColor(.red)
+                        .foregroundColor(Color(red: 255 / 255, green: 160 / 255, blue: 160 / 255))
                         .offset(y: -Spacing() * -7.5)
+                        .offset(y: -self.value)
+                        .animation(.spring())
                 }
                 
                 Spacer()
                 
                 HStack {
                     CustomButtonAuthView(action: self.signUp) {
-                        Text("Sign In")
+                        Text("Sign Up")
                     }
-                    .offset(x: -Spacing() * -4.0, y: -Spacing() * 0.0)
+                    .offset(y: -self.value)
+                    .animation(.spring())
+                    .offset(x: -Spacing() * -3.9, y: -Spacing() * 0.0)
                     .shadow(color: Color(red: 147 / 255, green: 156 / 255, blue: 238 / 255),
                             radius: 180.0)
                         .animation(Animation.easeInOut(duration: 0.5)
